3d31a7147e138756732bc8b83ce54f6418ef2e03
chaitanya-codes <chaitanyabajaj12@gmail.com>
2026-08-29 20:56:21 +0000

    refactor: optimize parser exports and routing

--- backend/src/parsers/index.js
+++ backend/src/parsers/index.js
@@ -1,11 +1,11 @@
 import { parseJavaScript } from './javascriptParser.js';
 import { parsePython } from './pythonParser.js';
 
-export function parseFile(fileInfo, allFiles) {
+export function parseFile(fileInfo, allFiles, aliases = []) {
   switch (fileInfo.language) {
     case 'javascript':
     case 'typescript':
-      return parseJavaScript(fileInfo, allFiles);
+      return parseJavaScript(fileInfo, allFiles, aliases);
     case 'python':
       return parsePython(fileInfo, allFiles);
     default:
